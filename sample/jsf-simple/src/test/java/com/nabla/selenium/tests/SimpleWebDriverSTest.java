282989e20300992c61c0c87051da18a9de0f51d7
Alban Andrieu <alban.andrieu@misys.com>
2015-04-10 15:45:39 +0200

    Improve tests

--- sample/jsf-simple/src/test/java/com/nabla/selenium/tests/SimpleWebDriverSTest.java
+++ sample/jsf-simple/src/test/java/com/nabla/selenium/tests/SimpleWebDriverSTest.java
@@ -7,8 +7,8 @@ import java.net.BindException;
 import java.net.ServerSocket;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
@@ -17,6 +17,8 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.server.RemoteControlConfiguration;
 import org.openqa.selenium.server.SeleniumServer;
 import org.openqa.selenium.support.ui.ExpectedCondition;
@@ -28,66 +30,75 @@ import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium;
 
 public class SimpleWebDriverSTest
 {
-    public static final String FORM_ID              = "hello-form";
-    public static final String SUBMIT_BUTTON_ID     = FORM_ID + ":submit-text";
-    public static final String INPUT_TEXT_ID        = FORM_ID + ":input-text";
+    public static final String         FORM_ID              = "hello-form";
+    public static final String         SUBMIT_BUTTON_ID     = FORM_ID + ":submit-text";
+    public static final String         INPUT_TEXT_ID        = FORM_ID + ":input-text";
     // private static final String DEFAULT_CHROMEDRIVER = "C:\\chromedriver\\chromedriver.exe"; // "/var/lib/chromedriver"
     // private static final String DEFAULT_FIREFOXBIN = "C:\\Program Files\\Mozilla Firefox\\firefox.exe"; // "/usr/lib/firefox/firefox"
-    public static final String DEFAULT_CHROMEDRIVER = "/var/lib/chromedriver";                      // "C:\\chromedriver\\chromedriver.exe"
-    public static final String DEFAULT_FIREFOXBIN   = "/usr/lib/firefox/firefox";                   // "C:\\Program Files\\Mozilla Firefox\\firefox.exe"
+    public static final String         DEFAULT_CHROMEDRIVER = "/var/lib/chromedriver";                      // "C:\\chromedriver\\chromedriver.exe"
+    public static final String         DEFAULT_FIREFOXBIN   = "/usr/lib/firefox/firefox";                   // "C:\\Program Files\\Mozilla Firefox\\firefox.exe"
 
-    public static final String DEFAULT_HOST         = "localhost";
-    public static final String DEFAULT_PORT         = "8180";
-    public static final String DEFAULT_URL          = "http://" + DEFAULT_HOST + ":" + DEFAULT_PORT;
-    public static final String PAGE_TO_LOAD_TIMEOUT = "30000";
-    private WebDriver          driver;
-    private String             baseUrl              = DEFAULT_URL;
-    private String             chromeDriver         = DEFAULT_CHROMEDRIVER;
-    private String             firefoxBin           = DEFAULT_FIREFOXBIN;
-    private boolean            acceptNextAlert      = true;
-    private StringBuffer       verificationErrors   = new StringBuffer();
-    private DefaultSelenium    selenium;
+    public static final String         DEFAULT_HOST         = "localhost";
+    public static final String         DEFAULT_PORT         = "8180";
+    public static final String         DEFAULT_URL          = "http://" + DEFAULT_HOST + ":" + DEFAULT_PORT;
+    public static final String         PAGE_TO_LOAD_TIMEOUT = "30000";
+    private static WebDriver           DRIVER;
+    private static String              BASE_URL             = DEFAULT_URL;
+    private static String              CHROME_DRIVER        = DEFAULT_CHROMEDRIVER;
+    private static String              FIREFOX_BIN          = DEFAULT_FIREFOXBIN;
+    private boolean                    acceptNextAlert      = true;
+    private static StringBuffer        _VERIFICATION_ERRORS = new StringBuffer();
+    private static DefaultSelenium     SELENIUM;
+
+    private static DesiredCapabilities CAPABILITIES;
 
     // public static SeleniumServer server;
 
-    @Before
-    public void setUp() throws Exception
+    @BeforeClass
+    public static void setUp() throws Exception
     {
 
-        baseUrl = System.getProperty("webdriver.base.url");
+        BASE_URL = System.getProperty("webdriver.base.url");
 
-        if (null == baseUrl)
+        if (null == BASE_URL)
         {
             System.out.println("Use default webdriver.base.url");
-            baseUrl = DEFAULT_URL;
-            System.setProperty("webdriver.base.url", baseUrl);
+            BASE_URL = DEFAULT_URL;
+            System.setProperty("webdriver.base.url", BASE_URL);
         }
-        System.out.println("webdriver.base.url is : " + baseUrl + "\n");
+        System.out.println("webdriver.base.url is : " + BASE_URL + "\n");
 
-        chromeDriver = System.getProperty("webdriver.chrome.driver");
-        if (null == chromeDriver)
+        CHROME_DRIVER = System.getProperty("webdriver.chrome.driver");
+        if (null == CHROME_DRIVER)
         {
             System.out.println("Use default webdriver.base.url");
-            chromeDriver = DEFAULT_CHROMEDRIVER;
-            System.setProperty("webdriver.chrome.driver", chromeDriver);
+            CHROME_DRIVER = DEFAULT_CHROMEDRIVER;
+            System.setProperty("webdriver.chrome.driver", CHROME_DRIVER);
         }
-        System.out.println("webdriver.chrome.driver is : " + chromeDriver + "\n");
+        System.out.println("webdriver.chrome.driver is : " + CHROME_DRIVER + "\n");
 
-        firefoxBin = System.getProperty("webdriver.firefox.bin");
-        if (null == firefoxBin)
+        FIREFOX_BIN = System.getProperty("webdriver.firefox.bin");
+        if (null == FIREFOX_BIN)
         {
             System.out.println("Use default webdriver.firefox.bin");
-            firefoxBin = DEFAULT_FIREFOXBIN;
-            System.setProperty("webdriver.firefox.bin", firefoxBin);
+            FIREFOX_BIN = DEFAULT_FIREFOXBIN;
+            System.setProperty("webdriver.firefox.bin", FIREFOX_BIN);
         }
-        System.out.println("webdriver.firefox.bin is : " + firefoxBin + "\n");
+        System.out.println("webdriver.firefox.bin is : " + FIREFOX_BIN + "\n");
         // ProfilesIni allProfiles = new ProfilesIni();
         // FirefoxProfile profile = allProfiles.getProfile("Selenium");
         // FirefoxProfile profile = new FirefoxProfile();
         // FirefoxBinary binary = new FirefoxBinary(new File(firefoxBin));
         // driver = new FirefoxDriver(binary, profile);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("no-sandbox");
 
-        driver = new ChromeDriver();
+        CAPABILITIES.setCapability(ChromeOptions.CAPABILITY, options);
+
+        // capabilities = DesiredCapabilities.chrome();
+        CAPABILITIES.setJavascriptEnabled(true);
+
+        DRIVER = new ChromeDriver(CAPABILITIES);
         // driver = new FirefoxDriver(profile);
         // driver = new HtmlUnitDriver(true);
 
@@ -97,11 +108,11 @@ public class SimpleWebDriverSTest
         // WebDriver augmentedDriver = new Augmenter().augment(driver);
         // File screenshot = ((TakesScreenshot) augmentedDriver).getScreenshotAs(OutputType.FILE);
 
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        DRIVER.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         // driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
-        driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+        DRIVER.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
         // driver.manage().window().setSize(new Dimension(1920, 1080));
-        selenium = new WebDriverBackedSelenium(driver, baseUrl);
+        SELENIUM = new WebDriverBackedSelenium(DRIVER, BASE_URL);
 
         Thread.sleep(10000); // 10 s
         // screenshot.
@@ -119,15 +130,15 @@ public class SimpleWebDriverSTest
     @Test
     public void testSimpleS() throws Exception
     {
-        driver.get(baseUrl + "/welcome/hello.xhtml");
-        selenium.waitForPageToLoad(PAGE_TO_LOAD_TIMEOUT);
+        DRIVER.get(BASE_URL + "/welcome/hello.xhtml");
+        SELENIUM.waitForPageToLoad(PAGE_TO_LOAD_TIMEOUT);
         // WebElement myDynamicElement = (new WebDriverWait(driver, 20)).until(ExpectedConditions.presenceOfElementLocated(By.id("j_idt8")));
-        assertEquals("JSF 2.0 Hello World Example - hello.xhtml", driver.findElement(By.cssSelector("h3")).getText());
-        driver.findElement(By.name(INPUT_TEXT_ID)).clear();
-        driver.findElement(By.name(INPUT_TEXT_ID)).sendKeys("Test me !!!");
+        assertEquals("JSF 2.0 Hello World Example - hello.xhtml", DRIVER.findElement(By.cssSelector("h3")).getText());
+        DRIVER.findElement(By.name(INPUT_TEXT_ID)).clear();
+        DRIVER.findElement(By.name(INPUT_TEXT_ID)).sendKeys("Test me !!!");
 
         // wait for the application to get fully loaded
-        WebElement findOwnerLink = (new WebDriverWait(driver, 5)).until(new ExpectedCondition<WebElement>()
+        WebElement findOwnerLink = (new WebDriverWait(DRIVER, 5)).until(new ExpectedCondition<WebElement>()
         {
             public WebElement apply(WebDriver d)
             {
@@ -138,25 +149,25 @@ public class SimpleWebDriverSTest
 
         findOwnerLink.click();
 
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebDriverWait wait = new WebDriverWait(DRIVER, 10);
         wait.until(ExpectedConditions.elementToBeClickable(By.name(SUBMIT_BUTTON_ID)));
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        DRIVER.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
-        driver.findElement(By.name(SUBMIT_BUTTON_ID)).click();
+        DRIVER.findElement(By.name(SUBMIT_BUTTON_ID)).click();
 
-        assertEquals("JSF 2.0 Hello World Example - welcome.xhtml", driver.findElement(By.cssSelector("h3")).getText());
-        assertEquals("Welcome Test me !!!", driver.findElement(By.cssSelector("h4")).getText());
+        assertEquals("JSF 2.0 Hello World Example - welcome.xhtml", DRIVER.findElement(By.cssSelector("h3")).getText());
+        assertEquals("Welcome Test me !!!", DRIVER.findElement(By.cssSelector("h4")).getText());
     }
 
-    @After
-    public void tearDown() throws Exception
+    @AfterClass
+    public static void tearDown() throws Exception
     {
         // stopSeleniumServer(server, selenium);
-        if (null != driver)
+        if (null != DRIVER)
         {
-            driver.quit();
+            DRIVER.quit();
         }
-        String verificationErrorString = verificationErrors.toString();
+        String verificationErrorString = _VERIFICATION_ERRORS.toString();
         if (!"".equals(verificationErrorString))
         {
             fail(verificationErrorString);
@@ -167,7 +178,7 @@ public class SimpleWebDriverSTest
     {
         try
         {
-            driver.findElement(by);
+            DRIVER.findElement(by);
             return true;
         } catch (NoSuchElementException e)
         {
@@ -179,7 +190,7 @@ public class SimpleWebDriverSTest
     {
         try
         {
-            driver.switchTo().alert();
+            DRIVER.switchTo().alert();
             return true;
         } catch (NoAlertPresentException e)
         {
@@ -191,7 +202,7 @@ public class SimpleWebDriverSTest
     {
         try
         {
-            Alert alert = driver.switchTo().alert();
+            Alert alert = DRIVER.switchTo().alert();
             String alertText = alert.getText();
             if (acceptNextAlert)
             {
