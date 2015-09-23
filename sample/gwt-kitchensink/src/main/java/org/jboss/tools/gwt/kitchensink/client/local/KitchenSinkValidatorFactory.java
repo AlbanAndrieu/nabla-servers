a9d3a8144dab36fc1310c8cdd13985e1864852ba
Alban Andrieu <alban.andrieu@mysis.com>
2015-09-23 04:02:18 -0500

    gwt not ready for upgrade

--- sample/gwt-kitchensink/src/main/java/org/jboss/tools/gwt/kitchensink/client/local/KitchenSinkValidatorFactory.java
+++ sample/gwt-kitchensink/src/main/java/org/jboss/tools/gwt/kitchensink/client/local/KitchenSinkValidatorFactory.java
@@ -1,5 +1,6 @@
 package org.jboss.tools.gwt.kitchensink.client.local;
 
+//import javax.validation.ParameterNameProvider;
 import javax.validation.Validator;
 import javax.validation.groups.Default;
 
@@ -32,4 +33,20 @@ public final class KitchenSinkValidatorFactory extends AbstractGwtValidatorFacto
     {
         return GWT.create(GwtValidator.class);
     }
+
+    /*
+    @Override
+    public ParameterNameProvider getParameterNameProvider()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void close()
+    {
+        // TODO Auto-generated method stub
+
+    }
+    */
 }
