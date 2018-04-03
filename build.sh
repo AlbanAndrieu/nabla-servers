c9f655de4e73ab3c4e15d727c73a087a310403bc
Andrieu, Alban <alban.andrieu@misys.com>
2018-04-04 01:40:41 +0200

    juniy 4.10

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -xv
 
 #./clean.sh
 
-mvn clean install -Dserver=jetty9x -Darquillian=arq-jetty-embedded
+mvn clean install -Dserver=jetty9x -Darquillian=arq-jetty-embedded  -Psample
 #-Parq-jetty-embedded,jetty9x
 
 exit 0
